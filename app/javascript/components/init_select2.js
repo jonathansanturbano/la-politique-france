6f18c1267ffbbbe42377141c60a4be1ac2437c19
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-11-15 17:14:28 +0100

    most seeds created missing avis and some sous thematiques

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,10 +2,14 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
-  $('.multi').select2({
-    closeOnSelect: false
-  });
+  if(document.querySelector('.select2')){
+    $('.select2').select2();
+  };
+  if(document.querySelector('.multi')){
+    $('.multi').select2({
+      closeOnSelect: false
+    });
+  };
 };
 
 export { initSelect2 };
