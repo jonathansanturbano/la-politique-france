c98d81034439ac21476ae0ce42aa74d506839b97
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2021-05-05 17:36:52 +0200

    hide avis sous thematique on parti show when there is no content

--- app/controllers/avis_thematiques_controller.rb
+++ app/controllers/avis_thematiques_controller.rb
@@ -5,6 +5,6 @@ class AvisThematiquesController < ApplicationController
     parti_id = @avis_thematique.parti_id
     thematique_id = @avis_thematique.thematique_id
     sous_thematiques = SousThematique.all.where(thematique_id: thematique_id)
-    @avis_sous_thematiques = AvisSousThematique.all.where(sous_thematique_id: sous_thematiques, parti: parti_id)
+    @avis_sous_thematiques = AvisSousThematique.all.where(sous_thematique_id: sous_thematiques, parti: parti_id).with_rich_text_rich_avis
   end
 end
